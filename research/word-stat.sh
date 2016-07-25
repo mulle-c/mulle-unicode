d3fb5a83025f9d26a2e015217b9293704930ed85
Nat! <nat@codeon.de>
2016-07-25 16:51:58 +0200

    Improve some tests

--- research/word-stat.sh
+++ research/word-stat.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 
-find . -name "*.[hm]" -exec grep -o -E '\w+' {} \; | sort > words
-sort < words > words.unique
-uniq -c words | sort -n -r > words.stats
+if [ ! -f words ]
+then
+   find . -name "*.[hm]" -exec grep -o -E '\w+' {} \; | sort > words
+fi
+
+# remove small strings that are used by char7
+
+sed '/^.$/d;/^..$/d;/^...$/d;/^....$/d;/^.....$/d;/^......$/d;/^.......$/d;/^........$/d' < words > words.large
+sort -u < words.large > words.unique
+uniq -c words.large | sort -n -r > words.stats
 egrep -o -E '.' < words.unique | sort | uniq -c | sort -n -r > char.stats
