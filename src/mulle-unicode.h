65d9c8870a35193a947c79789f99a1ed60d9cc66
Nat! <nat@mulle-kybernetik.com>
2025-04-22 21:18:44 +0200

    up da version and minor edits

--- src/mulle-unicode.h
+++ src/mulle-unicode.h
@@ -37,7 +37,7 @@
 #ifndef mulle_unicode_h__
 #define mulle_unicode_h__
 
-#define MULLE__UNICODE_VERSION  ((2UL << 20) | (4 << 8) | 9)
+#define MULLE__UNICODE_VERSION  ((2UL << 20) | (4 << 8) | 10)
 
 #include "include.h"
 
