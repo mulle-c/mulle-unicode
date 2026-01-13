5bde102d26677711b2685e0b134b6228ee99ff7c
Nat! <nat@mulle-kybernetik.com>
2026-01-13 22:35:47 +0100

    docs: improve quickstart instructions in README
    * added vibecoding activation step to quickstart workflow
    * clarified that quickstart users should skip the Add section
    
    Updated .mulle/share/sde/version files for c, c-cmake, and c-demo extensions.

--- test/.mulle/etc/env/environment-host-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -178,3 +178,21 @@ export MULLE__THREAD_URL="https://github.com/mulle-concurrent/mulle-thread.git"
 export MULLE__THREAD_BRANCH="prerelease"
 
 
+#
+# mulle-dtostr nodetype: ${MULLE__DTOSTR_NODETYPE:-tar}
+#
+export MULLE__DTOSTR_NODETYPE="git"
+
+
+#
+# mulle-dtostr url: ${MULLE__DTOSTR_URL:-https://github.com/mulle-core/mulle-dtostr/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__DTOSTR_URL="https://github.com/mulle-core/mulle-dtostr.git"
+
+
+#
+# mulle-dtostr branch: ${MULLE__DTOSTR_BRANCH}
+#
+export MULLE__DTOSTR_BRANCH="prerelease"
+
+
